4c9e25435d1ce74bb453ef394fb06f2f807fbb33
itsmeit <buivanloi.2010@gmail.com>
2023-04-15 06:53:21 +0700

    Change logic so as not to affect performance

--- filter-post-date.php
+++ filter-post-date.php
@@ -12,65 +12,71 @@ Text Domain: filter-post-date
 Copyright 2023 itsmeit.co (email: buivanloi.2010@gmail.com)
 */
 
-class FilterPostByDate {
+class FilterPostByDate
+{
 
-    function __construct() {
+    public function __construct()
+    {
+        add_action('init', array($this, 'initFilterPostByDateAdmin'), 99);
+    }
+
+    public function initFilterPostByDateAdmin()
+    {
         global $pagenow;
-        if (is_admin() && in_array($pagenow, array('edit.php', 'upload.php', 'post.php', 'post-new.php'))) {
+        if (is_admin() && in_array($pagenow, array('edit.php', 'upload.php'))) {
             add_filter('months_dropdown_results', '__return_empty_array');
             add_action('admin_enqueue_scripts', array($this, 'jqueryui'));
             add_action('restrict_manage_posts', array($this, 'form'));
-            add_action('pre_get_posts', array($this, 'filterquery'));
+            add_action('pre_get_posts', array($this, 'filterQuery'));
         }
     }
 
-    function jqueryui() {
+    public function jqueryui()
+    {
         wp_enqueue_style('jquery-ui', '//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.min.css');
         wp_enqueue_script('jquery-ui-datepicker');
     }
 
-    function form(){
+    public function form()
+    {
 
         $from = (isset($_GET['itsmeitDateFrom']) && $_GET['itsmeitDateFrom']) ? $_GET['itsmeitDateFrom'] : '';
         $to = (isset($_GET['itsmeitDateTo']) && $_GET['itsmeitDateTo']) ? $_GET['itsmeitDateTo'] : '';
 
         echo '<style>
-        input[name="itsmeitDateFrom"], input[name="itsmeitDateTo"]{
-            line-height: 28px;
-            height: 28px;
-            margin: 0;
-            width:125px;
-        }
-        </style>
-        
-        <input type="text" name="itsmeitDateFrom" placeholder="Date From" value="' . esc_attr($from) . '" />
-        <input type="text" name="itsmeitDateTo" placeholder="Date To" value="' . esc_attr($to) . '" />
-    
-        <script>
-        jQuery( function($) {
-            var from = $(\'input[name="itsmeitDateFrom"]\'),
-                to = $(\'input[name="itsmeitDateTo"]\');
+		input[name="itsmeitDateFrom"], input[name="itsmeitDateTo"]{
+			line-height: 28px;
+			height: 28px;
+			margin: 0;
+			width:125px;
+		}
+		</style>
+		
+		<input type="text" name="itsmeitDateFrom" placeholder="Date From" value="' . esc_attr($from) . '" />
+		<input type="text" name="itsmeitDateTo" placeholder="Date To" value="' . esc_attr($to) . '" />
+	
+		<script>
+		jQuery( function($) {
+			var from = $(\'input[name="itsmeitDateFrom"]\'),
+			    to = $(\'input[name="itsmeitDateTo"]\');
 
-            $( \'input[name="itsmeitDateFrom"], input[name="itsmeitDateTo"]\' ).datepicker( {dateFormat : "yy-mm-dd"} );
-                from.on( \'change\', function() {
-                to.datepicker( \'option\', \'minDate\', from.val() );
-            });
-                
-            to.on( \'change\', function() {
-                from.datepicker( \'option\', \'maxDate\', to.val() );
-            });
-            
-        });
-        </script>';
+			$( \'input[name="itsmeitDateFrom"], input[name="itsmeitDateTo"]\' ).datepicker( {dateFormat : "yy-mm-dd"} );
+    			from.on( \'change\', function() {
+				to.datepicker( \'option\', \'minDate\', from.val() );
+			});
+				
+			to.on( \'change\', function() {
+				from.datepicker( \'option\', \'maxDate\', to.val() );
+			});
+			
+		});
+		</script>';
 
     }
 
-    function filterquery($admin_query){
-        global $pagenow;
-        if (
-            is_admin()
-            && $admin_query->is_main_query()
-            && in_array($pagenow, array('edit.php', 'upload.php'))
+    public function filterQuery($admin_query)
+    {
+        if ($admin_query->is_main_query()
             && (!empty($_GET['itsmeitDateFrom']) || !empty($_GET['itsmeitDateTo']))
         ) {
             $admin_query->set(
